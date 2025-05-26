1090bb59830fbe9ec9e977a590c45203a66b9429
Popov85 <andrey.popov85@gmail.com>
2025-05-26 14:02:37 +0300

    Init

---
+++ src/test/java/com/simulations/CoreSimulation.java
@@ -0,0 +1,29 @@
+package com.simulations;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class CoreSimulation extends Simulation {
+    HttpProtocolBuilder httpProtocol = http
+            .baseUrl("http://localhost:8080");
+
+    ScenarioBuilder scn = scenario("Basic Hello Test")
+            .exec(
+                    http("GET /ping")
+                            .get("/ping")
+                            .check(status().saveAs("status"))
+            );
+
+    {
+        setUp(
+                scn.injectOpen(atOnceUsers(1))
+        ).protocols(httpProtocol);
+    }
+
+}
